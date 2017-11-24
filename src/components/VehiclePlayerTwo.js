09a24e4fac11aa67402194abe6506d229b68106d
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-24 21:47:28 +0100

    style updated

--- src/components/VehiclePlayerTwo.js
+++ src/components/VehiclePlayerTwo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class VehiclePlayerTwo extends Component{
   render(){
     return (
-      <div>
+      <div >
         <img src={"../space.png"} style={{position:"absolute", top:"40%", left:"72%", width:"65px"}} />
         <h3 style = {{ color:"white", position:"absolute", left:"68%" }}>Imperial Speeder Bike</h3>
       </div>
