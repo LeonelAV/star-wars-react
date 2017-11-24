2822a792db37873309008521f35259374a659116
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-24 13:48:00 +0100

    some style gold component

--- src/components/Gold.js
+++ src/components/Gold.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Gold extends Component{
   render(){
     return (
-      <div>
-        <img src={"../../public/gold.png"}></img>
+      <div >
+        <img style={{transform: "rotate(-90deg)", width:"7%"}} src={"../gold.png"}/>
         <h2>500kg</h2>
       </div>
     )
