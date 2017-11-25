a0dc7dac1ec431044417304d993506300d6590ef
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-25 17:37:50 +0100

    distance choosed randomly

--- src/components/Distance.js
+++ src/components/Distance.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 class Distance extends Component{
+  constructor(props){
+    super(props);
+    this.state = {distance: 0}
+  }
+
+componentWillMount(){
+  this.setState({ distance: Math.floor(Math.random()*2000) })
+}
+
   render(){
     return (
       <div style={{ position:"absolute", left:"45.5%", top:"60%" }}>
         <img style={{width:60, float: "left"}} src={"../distance.png"}/>
-        <h2 style={{float:"left", marginLeft:20, color:"white", fontWeight:"200"}}>1500km</h2>
+        <h2 style={{float:"left", marginLeft:20, color:"white", fontWeight:"200"}}>{this.state.distance}km</h2>
       </div>
     )
   }
