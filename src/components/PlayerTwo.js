1666404459e127a349db29e20d5467db3f44c0f7
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-25 17:10:40 +0100

    player two  choosed randomly

--- src/components/PlayerTwo.js
+++ src/components/PlayerTwo.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 class PlayerTwo extends Component {
+  constructor(props){
+    super(props);
+    this.state = {name:[], id: 0, vehicles:[]}
+  }
+
+    componentDidMount() {
+    fetch('https://swapi.co/api/people/')
+      .then(results => {
+        return results.json();
+      }).then(data => {
+          let ourArray = data.results.length // Total number of players we get from the Api
+          this.setState({ 
+            name: data.results[Math.floor(Math.random()*ourArray)].name
+          })
+ })
+}
+
   render(){
     return (
       <div >
         <h2 style={{color:"#EE4235", margin: 0}}>Player 2</h2>
-        <h2 style={{color:"white", margin: 0, fontWeight:400}}>Darth Vader</h2>
+        <h2 style={{color:"white", margin: 0, fontWeight:400}}>{this.state.name}</h2>
         <h4 style={{color:"white", margin: 0, fontWeight:400}}>Human</h4>
       </div>
     )
