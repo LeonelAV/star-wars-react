b8dc005cd87dd05f7e750e7dbfac2ef8b9564eff
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-25 17:23:51 +0100

    gold amount randomly

--- src/components/Gold.js
+++ src/components/Gold.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 class Gold extends Component{
+  constructor(props){
+    super(props);
+    this.state={ amountOfGold: 0}
+  }
+
+  componentWillMount(){
+    this.setState({ amountOfGold: Math.floor(Math.random()*1000)})
+  }
+
   render(){
     return (
       <div style={{ position:"relative", left:"45%" }}>
         <img style={{transform: "rotate(-90deg)", width:65, float: "left"}} src={"../gold.png"}/>
-        <h2 style={{float:"left", marginLeft:20, color:"white", fontWeight:"200"}}>500kg</h2>
+        <h2 style={{float:"left", marginLeft:20, color:"white", fontWeight:"200"}}>{this.state.amountOfGold}kg</h2>
       </div>
     )
   }
