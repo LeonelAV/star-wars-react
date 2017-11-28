84ea77dddcf51d021c213fec2b58a25cd1adc70a
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-28 21:32:52 +0100

    change fetch requestAPI into index.js

--- src/components/PlayerOne.js
+++ src/components/PlayerOne.js
@@ -3,57 +3,57 @@ import VehiclePlayerOne from './VehiclePlayerOne';
 import DataVehicleOne from './DataVehicleOne';
 
 class PlayerOne extends Component {
-  constructor(props){
-    super(props);
-    this.state = {name:[], id: 0, vehicles:"", vehicleName: "", speed:"", cargo: ""}
-  }
+  // constructor(props){
+  //   super(props);
+  //   // this.state = {name:[], id: 0, vehicles:"", vehicleName: "", speed:"", cargo: ""}
+  // }
 
- componentDidMount() {
-    let randomPage= Math.floor(Math.random()*9) + 1 // this is a random number to choose one page of the API, there 9 in total
-    fetch('https://swapi.co/api/people/?page=' + randomPage)// API peticion for the player
-      .then(results => {
-        return results.json();
-       })
-      .then(data => {
-        let ourArray = data.results.length // Total number of players we get from the Api
-        let playerIndex = Math.floor(Math.random()*ourArray)// to choose a random player
-        let lenghtArrVehicles= data.results[playerIndex].vehicles.length
-        let vehicleIndex = Math.floor(Math.random()*lenghtArrVehicles)
-        this.setState({ 
-          name: data.results[playerIndex].name, 
-          id: playerIndex,
-          vehicles: data.results[playerIndex].vehicles[vehicleIndex]
-        })
+//  componentDidMount() {
+//     let randomPage= Math.floor(Math.random()*9) + 1 // this is a random number to choose one page of the API, there 9 in total
+//     fetch('https://swapi.co/api/people/?page=' + randomPage)// API peticion for the player
+//       .then(results => {
+//         return results.json();
+//        })
+//       .then(data => {
+//         let ourArray = data.results.length // Total number of players we get from the Api
+//         let playerIndex = Math.floor(Math.random()*ourArray)// to choose a random player
+//         let lenghtArrVehicles= data.results[playerIndex].vehicles.length
+//         let vehicleIndex = Math.floor(Math.random()*lenghtArrVehicles)
+//         this.setState({ 
+//           name: data.results[playerIndex].name, 
+//           id: playerIndex,
+//           vehicles: data.results[playerIndex].vehicles[vehicleIndex]
+//         })
 
-     const urlVehicles=this.state.vehicles
+//      const urlVehicles=this.state.vehicles
      
-     fetch(urlVehicles) //API peticion for the vehicle
-    .then(results => {
-      return results.json();
-      })
-     .catch(error => {
-       console.log('Is afucking error');
-     })
-    .then(data => {
-      this.setState({
-        vehicleName: data.name,
-        speed: data.max_atmosphering_speed,
-        cargo: data.cargo_capacity
-      })
-  })
- })
-}
+//      fetch(urlVehicles) //API peticion for the vehicle
+//     .then(results => {
+//       return results.json();
+//       })
+//      .catch(error => {
+//        console.log('Is afucking error');
+//      })
+//     .then(data => {
+//       this.setState({
+//         vehicleName: data.name,
+//         speed: data.max_atmosphering_speed,
+//         cargo: data.cargo_capacity
+//       })
+//   })
+//  })
+// }
 
 
   render(){
     return (
       <div >
         <h2 style={{color:"#2196F3", margin: 0}}>Player 1</h2>
-        <h2 style={{color:"white", margin: 0, fontWeight:400}}>{this.state.name}</h2>
+        <h2 style={{color:"white", margin: 0, fontWeight:400}}>{this.props.nameOne.name}</h2>
         <h4 style={{color:"white", margin: 0, fontWeight:400}}>Human</h4>
        <div className="player-one" style={{ position: "relative", left:"9.5%", top: "150%", width:"148%" }}>
-         <VehiclePlayerOne vehicleName= {this.state.vehicleName} />
-         <DataVehicleOne speed= {this.state.speed} cargo={this.state.cargo}/>
+         <VehiclePlayerOne vehicleName=""  />
+         <DataVehicleOne speed= "" cargo=""/>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -14,14 +14,64 @@ import DataVehicleTwo from './components/DataVehicleTwo';
 import YouWon from './components/YouWon';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { playerOne: [], playerTwo: []}
+  }
+
+  componentDidMount(){
+    // API peticion for player ONE
+    let randomPage= Math.floor(Math.random()*9) + 1 // this is a random number to choose one page of the API, there 9 in total
+    fetch('https://swapi.co/api/people/?page=' + randomPage)// API peticion for the player
+      .then(results => {
+        return results.json();
+       })
+      .then(data => {
+        let arrLenght = data.results.length // Total number of players we get from the Api
+        let playerIndex = Math.floor(Math.random()*arrLenght)// to choose a random player 
+        this.setState({ 
+          playerOne: data.results[playerIndex]
+        })
+        // console.log(this.state.playerOne)
+  })
+    //API peticion for Player Two => we don't use the same because of the pagination of the json, like that is less probability of getting the same player
+    let randomPageTwo= Math.floor(Math.random()*9) + 1 // this is a random number to choose one page of the API, there 9 in total
+    fetch('https://swapi.co/api/people/?page=' + randomPageTwo)// API peticion for the player
+      .then(results => {
+        return results.json();
+       })
+      .then(data => {
+        let arrLenght = data.results.length // Total number of players we get from the Api
+        let playerIndex = Math.floor(Math.random()*arrLenght)// to choose a random player
+        let dataPlayers= data.results
+        console.log(playerIndex)
+        console.log(data.results[playerIndex].vehicles.length)
+         for(let value in dataPlayers){
+          if(dataPlayers.vehicles === []) {
+            dataPlayers.splice(1)
+           } 
+         }
+        
+        this.setState({ 
+          playerTwo: dataPlayers[playerIndex]
+        })
+        console.log(playerIndex)
+        console.log(data.results[playerIndex].vehicles.length)
+        console.log(data.results)
+        console.log(this.state.playerTwo) 
+  })
+    }
+
+
   render() {
+ 
     return (
       <div  className="App">
         <YouWon />
         <div style={{display:"flex", justifyContent:"space-evenly", position:"relative", top:100}}>
-          <PlayerOne />
+          <PlayerOne nameOne={this.state.playerOne}/>
           <h2 style={{color:"white", fontWeight:400}}>VS</h2>
-          <PlayerTwo />
+          <PlayerTwo nameTwo={this.state.playerTwo}/>
         </div>
         <Score />
         <Gold />
